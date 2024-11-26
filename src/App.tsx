3b637fb0c024a5f2e4fd1e3f042693a6c5f69260
Jake Nead <jakenead@gmail.com>
2024-11-25 20:31:45 -0800

    Use same naming scheme for avyReports across files

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,22 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [avyForecast, setAvyForecast] = useState<string | null>(null);
+  const [avyReport, setAvyReport] = useState<string | null>(null);
 
   useEffect(() => {
-    async function fetchAvyForecast() {
+    async function fetchAvyReport() {
       try {
         const response = await fetch("http://localhost:3000/revelstoke/report");
         if (!response.ok)
           throw new Error(`Uh oh, HTTP error! status ${response.status}`);
         const data: { avyReport: string } = await response.json();
 
-        setAvyForecast(data.avyReport);
+        setAvyReport(data.avyReport);
       } catch (err) {
         console.error("Error fetching avalanche forecast: ", err);
       }
     }
-    fetchAvyForecast();
+    fetchAvyReport();
   }, []);
   return (
     <>
@@ -31,11 +31,7 @@ function App() {
         src="https://avalanche.ca/map"
       ></iframe>
       <h2>Report</h2>
-      {avyForecast ? (
-        <p dangerouslySetInnerHTML={{ __html: avyForecast }} />
-      ) : (
-        <p>Loading avalanche report...</p>
-      )}
+      {avyReport ? <p>{avyReport}</p> : <p>Loading avalanche report...</p>}
       <h2>Weather</h2>
       <div>weather info goes here</div>
     </>
