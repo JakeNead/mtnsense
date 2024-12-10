ba83a4413a0a9cfd1156e5927eaf48ff47ca3529
Jake Nead <jakenead@gmail.com>
2024-12-10 13:36:45 -0800

    Wrap app in positioning containers

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,28 @@ import RogersPass from "./routes/RogersPass";
 import GrandTeton from "./routes/GrandTeton";
 import { BrowserRouter, Routes, Route } from "react-router";
 import { Provider } from "./components/ui/provider";
+import { Box, Center, Container, Flex } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
       <Provider>
         <BrowserRouter>
-          <Header />
-          <Routes>
-            <Route index element={<RogersPass />} />
-            <Route index path="/rogerspass" element={<RogersPass />} />
-            <Route index path="/grandteton" element={<GrandTeton />} />
-          </Routes>
+          <Box>
+            <Flex
+              flexDir="column"
+              align="center"
+              padding="0rem 1rem"
+              gap="1rem"
+            >
+              <Header />
+              <Routes>
+                <Route index element={<RogersPass />} />
+                <Route index path="rogerspass" element={<RogersPass />} />
+                <Route index path="grandteton" element={<GrandTeton />} />
+              </Routes>
+            </Flex>
+          </Box>
         </BrowserRouter>
       </Provider>
     </>
