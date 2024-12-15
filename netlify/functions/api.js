79a0f1032c8bf564ccb016185332ee25ea7e1ccd
Jake Nead <jakenead@gmail.com>
2024-12-14 16:36:08 -0800

    Use my api instead of test api

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,18 +1,4 @@
-// YOUR_BASE_DIRECTORY/netlify/functions/api.ts
-
-import express, { Router } from "express";
 import serverless from "serverless-http";
+import app from "../../server/server.js";
 
-const api = express();
-
-const router = Router();
-router.get("/hello", (req, res) => res.send("What up World!"));
-
-api.use("/api/", router);
-
-export const handler = serverless(api);
-
-// import serverless from "serverless-http";
-// import app from "../../server/server.js";
-
-// export const handler = serverless(app);
+export const handler = serverless(app);
