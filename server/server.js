4781908f752af604f2f6cfe8b69061a7fd57b315
Jake Nead <jakenead@gmail.com>
2024-12-15 10:52:20 -0800

    Create environment conditional

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 import express from "express";
-// import dotenv from "dotenv";
+import dotenv from "dotenv";
 import cors from "cors";
 import router from "./routes/router.js";
 
 dotenv.config();
 const app = express();
-// const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3001;
 
 const corsOptions = {
   origin: "https://mtnsense-forecasts.netlify.app",
@@ -16,8 +16,10 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/", router);
 
-// app.listen(port, () => {
-//   console.log(`Server running on http://localhost:${port}`);
-// });
+if (process.env.NODE_ENV !== "production") {
+  app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+  });
+}
 
 export default app;
