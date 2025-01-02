362b8e2a418ea3e86d326f504d13d9a128fc97e1
Jake Nead <jakenead@gmail.com>
2025-01-02 13:46:58 -0800

    Refactor to use api instead of scraping

--- netlify/functions/rogers-pass-avy.mjs
+++ netlify/functions/rogers-pass-avy.mjs
@@ -1,5 +1,3 @@
-import puppeteer from "puppeteer";
-import chromium from "@sparticuz/chromium";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { Response } from "node-fetch";
 
@@ -14,42 +12,29 @@ export default async () => {
     },
   });
 
-  chromium.setGraphicsMode = false;
-  chromium.setHeadlessMode = true;
+  const isLocal = !!process.env.CHROME_EXECUTABLE_PATH;
 
   try {
-    const isLocal = !!process.env.CHROME_EXECUTABLE_PATH;
-    browser = await puppeteer.launch({
-      args: isLocal ? puppeteer.defaultArgs() : chromium.args,
-      // defaultViewport: chromium.defaultViewport,
-      defaultViewport: { width: 1920, height: 3300 },
-      executablePath:
-        process.env.CHROME_EXECUTABLE_PATH || (await chromium.executablePath()),
-      headless: chromium.headless,
-    });
+    const response = await fetch(
+      "https://api.avalanche.ca/forecasts/:lang/products/point?lat=51.291168&long=-117.456425"
+    );
+    const data = await response.json();
 
-    const page = await browser.newPage();
+    const key = `rogers-pass-avy/latest.json`;
 
-    await page.goto("https://avalanche.ca/map?lat=51.291168&lng=-117.456425", {
-      waitUntil: "networkidle0",
-    });
-
-    const fileElement = await page.waitForSelector("#primary-drawer > div");
-
-    const screenshotBuffer = await fileElement.screenshot();
-
-    const key = `rogers-pass-avy/latest.png`;
-
-    const uploadParams = {
+    const params = {
       Bucket: process.env.S3_BUCKET_NAME,
       Key: key,
-      Body: screenshotBuffer,
-      ContentType: "image/png",
+      Body: JSON.stringify(data),
+      ContentType: "application/json",
     };
 
-    await s3.send(new PutObjectCommand(uploadParams));
+    await s3.send(new PutObjectCommand(params));
 
     await browser.close();
+
+    //Returning a Response locally throws an error
+    if (isLocal) return;
 
     return new Response("Scheduled Rogers Pass avy screenshot updated", {
       statusCode: 200,
@@ -59,6 +44,9 @@ export default async () => {
     if (browser) {
       await browser.close();
     }
+    //Returning a Response locally throws an error
+    if (isLocal) return;
+
     return new Response(
       "scheduled rogers pass avy function failed: " + error.message,
       { status: 500 }
