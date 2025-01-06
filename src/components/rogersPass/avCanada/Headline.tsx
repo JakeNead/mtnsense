5b1b3ad09fbe87acc4324715494055ac7c822ff3
Jake Nead <jakenead@gmail.com>
2025-01-05 18:51:57 -0800

    Use props

--- src/components/rogersPass/avCanada/Headline.tsx
+++ src/components/rogersPass/avCanada/Headline.tsx
@@ -1,7 +1,19 @@
-export default function Headline() {
+import { Report } from "../../../interfaces/AvyReport";
+import { Box } from "@chakra-ui/react";
+
+export default function Headline({ highlights }: Report) {
   return (
     <>
-      <div>TLDR</div>
+      <Box
+        dangerouslySetInnerHTML={{ __html: highlights }}
+        m="3rem 0rem"
+        fontSize="xl"
+        css={{
+          "& p": {
+            marginBottom: "1rem",
+          },
+        }}
+      />
     </>
   );
 }
