4b985562075945740a8184e995f4f8316142b309
Jake Nead <jakenead@gmail.com>
2025-01-14 14:11:07 -0800

    Add logs to scheduled function

--- netlify/functions/rogers-pass-min-reports.mjs
+++ netlify/functions/rogers-pass-min-reports.mjs
@@ -1,4 +1,4 @@
-import puppeteer from "puppeteer-core";
+import puppeteer from "puppeteer";
 import chromium from "@sparticuz/chromium";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { Response } from "node-fetch";
@@ -17,13 +17,9 @@ export default async () => {
     },
   });
 
-  const isLocal =
-    typeof process !== "undefined" &&
-    process.versions != null &&
-    process.versions.node != null &&
-    !!process.env.CHROME_EXECUTABLE_PATH;
-
   try {
+    const isLocal = !!process.env.CHROME_EXECUTABLE_PATH;
+
     browser = await puppeteer.launch({
       args: isLocal ? puppeteer.defaultArgs() : chromium.args,
       defaultViewport: chromium.defaultViewport,
@@ -33,37 +29,45 @@ export default async () => {
     });
 
     const page = await browser.newPage();
-    const baseUrl = "https://avalanche.ca";
-    await page.goto(`${baseUrl}/mountain-information-network/submissions`, {
-      waitUntil: "networkidle0",
-    });
+
+    await page.goto(
+      "https://avalanche.ca/mountain-information-network/submissions",
+      {
+        waitUntil: "networkidle0",
+      }
+    );
 
     // Find the Selkirk reports and get the first three links
-    const selkirkLinks = await page.evaluate((baseUrl) => {
+    const selkirkLinks = await page.evaluate(() => {
       const links = [];
       const rows = Array.from(document.querySelectorAll("tr"));
+      console.log(rows);
       rows.forEach((row) => {
         const cells = row.querySelectorAll("td");
         if (
           cells.length > 0 &&
           cells[cells.length - 2].textContent.trim() === "Selkirks"
         ) {
+          console.log("found a selkirk element");
           const anchor = cells[0].querySelector("a");
           if (anchor) {
             const href = anchor.getAttribute("href");
             if (href && href.startsWith("/")) {
-              links.push(baseUrl + href);
+              links.push("https://avalanche.ca" + href);
             }
           }
         }
       });
       return links.slice(0, 3); // Keep only the first three links
-    }, baseUrl);
+    });
+
+    console.log("selkrirksLinks: ", selkirkLinks);
 
     const reportContent = [];
 
     for (const link of selkirkLinks) {
       await page.goto(link, { waitUntil: "networkidle0" });
+      console.log("Visiting selkirks link");
 
       const content = await page.evaluate(() => {
         const getTextContent = (dtText) => {
