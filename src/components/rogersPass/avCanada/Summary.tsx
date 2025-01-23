d5e0ebcfd0d13ac0043680a6660d95ddfe8c59f4
Jake Nead <jakenead@gmail.com>
2025-01-23 11:34:18 -0800

    Handle missing content errors

--- src/components/rogersPass/avCanada/Summary.tsx
+++ src/components/rogersPass/avCanada/Summary.tsx
@@ -19,54 +19,56 @@ function capitalize(str: string) {
 export default function Summary({ summaries, confidence }: SummaryProps) {
   return (
     <>
-      {summaries.map((summary, index) => {
-        if (summary.type.value === "avalanche-summary") {
-          return (
-            <Box key={`avalanche-summary-${index}`}>
-              <Heading textAlign="center" margin="1rem">
-                Avalanche Summary
-              </Heading>
-              <Box
-                dangerouslySetInnerHTML={{
-                  __html: DOMPurify.sanitize(summary.content),
-                }}
-                css={{
-                  "& p": {
-                    marginBottom: ".8rem",
-                  },
-                }}
-              ></Box>
-            </Box>
-          );
-        }
-        if (summary.type.value === "snowpack-summary") {
-          return (
-            <Box key={`snowpack-summary-${index}`}>
-              <Heading textAlign="center" m="1rem">
-                Snowpack Summary
-              </Heading>
-              <Box
-                dangerouslySetInnerHTML={{
-                  __html: DOMPurify.sanitize(summary.content),
-                }}
-                css={{
-                  "& p": {
-                    marginBottom: ".8rem",
-                  },
-                }}
-              ></Box>
-            </Box>
-          );
-        }
-      })}
+      {summaries &&
+        summaries.map((summary, index) => {
+          if (summary.type.value === "avalanche-summary") {
+            return (
+              <Box key={`avalanche-summary-${index}`}>
+                <Heading textAlign="center" margin="1rem">
+                  Avalanche Summary
+                </Heading>
+                <Box
+                  dangerouslySetInnerHTML={{
+                    __html: DOMPurify.sanitize(summary.content),
+                  }}
+                  css={{
+                    "& p": {
+                      marginBottom: ".8rem",
+                    },
+                  }}
+                ></Box>
+              </Box>
+            );
+          }
+          if (summary.type.value === "snowpack-summary") {
+            return (
+              <Box key={`snowpack-summary-${index}`}>
+                <Heading textAlign="center" m="1rem">
+                  Snowpack Summary
+                </Heading>
+                <Box
+                  dangerouslySetInnerHTML={{
+                    __html: DOMPurify.sanitize(summary.content),
+                  }}
+                  css={{
+                    "& p": {
+                      marginBottom: ".8rem",
+                    },
+                  }}
+                ></Box>
+              </Box>
+            );
+          }
+        })}
       <Box>
         <Heading textAlign="center">Confidence</Heading>
         <Heading as="h3" fontSize="large">
           {capitalize(confidence.rating.value)}
         </Heading>
-        {confidence.statements.map((statement, index) => (
-          <Text key={index}> - {statement}</Text>
-        ))}
+        {confidence.statements &&
+          confidence.statements.map((statement, index) => (
+            <Text key={index}> - {statement}</Text>
+          ))}
       </Box>
     </>
   );
