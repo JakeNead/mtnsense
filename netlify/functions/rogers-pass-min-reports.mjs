0bce45613fd469f0aed061a11b49f8eb41545887
Jake Nead <jakenead@gmail.com>
2025-01-27 13:03:12 -0800

    Remove timeout

--- netlify/functions/rogers-pass-min-reports.mjs
+++ netlify/functions/rogers-pass-min-reports.mjs
@@ -42,7 +42,6 @@ export default async () => {
       "https://avalanche.ca/mountain-information-network/submissions",
       {
         waitUntil: "networkidle0",
-        timeout: 60000,
       }
     );
 
